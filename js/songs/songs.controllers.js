126ec93b7a8802f3af4b094307a32e13526f021b
Jonathan Polakoff <jdpolakoff@gmail.com>
2017-08-17 10:56:57 -0400

    added new song functionality

--- js/songs/songs.controllers.js
+++ js/songs/songs.controllers.js
@@ -1,6 +1,7 @@
 angular
 .module('wdi-radio')
 .controller('SongIndexController', [
+  '$state',
   'Song',
   SongIndexControllerFunction
 ])
@@ -9,11 +10,31 @@ angular
   'Song',
   SongShowControllerFunction
 ])
+.controller('SongNewController', [
+  '$state',
+  'Song',
+  SongNewControllerFunction
+])
 
-function SongIndexControllerFunction (Song) {
+function SongIndexControllerFunction ($state, Song) {
   this.songs = Song.query()
+  // this.newSong = new Song()
+  // this.create = function () {
+  //   this.newSong.$save(() => {
+  //     $state.go('songIndex', {}, { reload: true })
+  //   })
+  // }
 }
 
 function SongShowControllerFunction ($stateParams, Song) {
   this.song = Song.get({id: $stateParams.id})
+}
+
+function SongNewControllerFunction ($state, Song) {
+  this.newSong = new Song()
+  this.create = function () {
+    this.newSong.$save(() => {
+      $state.go('songIndex', {}, { reload: true })
+    })
+  }
 }
--- js/songs/songs.routes.js
+++ js/songs/songs.routes.js
@@ -4,13 +4,13 @@ angular
 .module('wdi-radio')
 .config([
 '$stateProvider',
-// '$locationProvider',
-// '$urlRouterProvider'
+'$locationProvider',
+'$urlRouterProvider',
   RouterFunction
   ])
 
-function RouterFunction ($stateProvider) {
-  // $locationProvider.html5mode(true)
+function RouterFunction ($stateProvider, $locationProvider, $urlRouterProvider) {
+  // $locationProvider.html5Mode(true)
     $stateProvider
       .state('songIndex', {
         url: '/songs',
@@ -18,11 +18,18 @@ function RouterFunction ($stateProvider) {
         controller: 'SongIndexController',
         controllerAs: 'vm'
       })
-      // $urlRouterProvider.otherwise('/songs')
+      .state('songNew', {
+        parent: 'songIndex',
+        url: '/new',
+        templateUrl: 'js/songs/songs.new.html',
+        controller: 'SongNewController',
+        controllerAs: 'vm'
+      })
       .state('songShow', {
         url: '/songs/:id',
         templateUrl: 'js/songs/songs.show.html',
         controller: 'SongShowController',
         controllerAs: 'vm'
       })
+      $urlRouterProvider.otherwise('/songs')
       }
